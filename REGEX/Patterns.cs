dac4548f5b2dff27ac3c6c8654e51b0dbd2de18b
peermohsin22 <peermohsin22@gmail.com>
2022-01-02 01:21:25 +0530

    Valid First Name

---
+++ REGEX/Patterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace REGEX
+{
+    class Patterns
+    {
+        public void Pattern()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            string s = @"^[A-Z]{1}[a-z]{3,}$";
+            Regex Name = new Regex(s);
+            Console.WriteLine($"Name is: {Name.Match(name)}");
+        }
+    }
+}
---
+++ REGEX/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace REGEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regex!");
+            Patterns patterns = new Patterns();
+            patterns.Pattern();
+        }
+    }
+}
