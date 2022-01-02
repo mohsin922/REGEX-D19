854d1641583225ce7b4b566f7c594e2fc6747d09
peermohsin22 <peermohsin22@gmail.com>
2022-01-02 12:14:43 +0530

    Validating by Testing

--- REGEX/Program.cs
+++ REGEX/Program.cs
@@ -6,27 +6,9 @@ namespace REGEX
     {
         static void Main(string[] args)
         {
-            string FirstName, LastName;
-            Console.Write("Enter First Name: ");
-            FirstName = Console.ReadLine();
-            Patterns.checkFirstName(FirstName);
-
-            Console.Write("Enter Last Name: ");
-            LastName = Console.ReadLine();
-            Patterns.checkLastName(LastName);
-            Console.WriteLine(FirstName + " " + LastName);
-
-            Console.Write("Enter your email addresss: ");
-            string email = Console.ReadLine();
-            Patterns.checkemail(email);
-
-            Console.Write("Enter your mobile number: ");
-            string number = Console.ReadLine();
-            Patterns.checknumber(number);
-
-            Console.Write("Enter your password: ");
-            string password = Console.ReadLine();
-            Patterns.checkPassword(password);
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
         }
     }
 }
